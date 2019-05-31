80691fa7c09ce4464ece33bd33079419e1925a78
lmdlll <2731786059@qq.com>
2019-05-31 22:28:27 +0800

    plus

---
+++ Pyhton/plus.py
@@ -0,0 +1,20 @@
+#阶乘
+
+#递归
+def plus(i):
+	if i==1:
+		return 1
+	return plus(i-1)*i
+
+#for循环
+def plus2(n):
+	j=1
+	for i in range(1,n+1):
+		j *= i
+	print j
+
+
+number = int(input("请输入一个正整数："))
+plus2(number)
+result1 = plus(number)
+print(result1)
