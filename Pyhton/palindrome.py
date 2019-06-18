589a8f48e601af5de47e1b5018ca8d46a728130f
lmdlll <2731786059@qq.com>
2019-06-18 22:06:06 +0800

    pal

---
+++ Pyhton/palindrome.py
@@ -0,0 +1,23 @@
+﻿#题目：判断回文数：一个5位数，判断它是不是回文数。
+#12321
+#123321
+
+# -*- coding: UTF-8 -*-
+
+class Palindrome:
+    def judge(self,a):
+        x = str(a)
+        flag = True
+        for i in range(len(x)/2):
+            if x[i] != x[-i - 1]:
+                flag = False
+                break
+        if flag:
+            print("%d 是一个回文数!" %a)
+        else:
+            print("%d 不是一个回文数!" %a)
+
+if __name__=='__main__':
+    pa=Palindrome()
+    a = int(raw_input("请输入一个数字:\n"))
+    pa.judge(a)
