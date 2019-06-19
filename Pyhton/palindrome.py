575f37e27a87790f67deeaaeea35a767bcf9d676
lmdlll <2731786059@qq.com>
2019-06-19 22:02:50 +0800

    pal

--- Pyhton/palindrome.py
+++ Pyhton/palindrome.py
@@ -17,7 +17,20 @@ class Palindrome:
         else:
             print("%d 不是一个回文数!" %a)
 
+    #反转字符串
+    def judge2(self,x):                
+        str_x = str(x)
+        str_y = ""
+        for i in str_x:
+            str_y = i + str_y 
+        if str_y == str_x:
+            print("%d 是一个回文数!" %a)
+        else:
+            print("%d 不是一个回文数!" %a)
+
+
 if __name__=='__main__':
     pa=Palindrome()
     a = int(raw_input("请输入一个数字:\n"))
     pa.judge(a)
+    pa.judge2(a)
