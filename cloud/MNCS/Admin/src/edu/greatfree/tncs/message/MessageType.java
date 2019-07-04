dd464a58cd5554e4549698f2b3d18bb9c9b08cab
lmdlll <2731786059@qq.com>
2019-07-04 22:38:55 +0800

    cloud

--- cloud/MNCS/Admin/src/edu/greatfree/tncs/message/MessageType.java
+++
@@ -1,9 +0,0 @@
-package edu.greatfree.tncs.message;
-
-// Created: 04/30/2019, Bing Li
-public class MessageType
-{
-	public final static int MERCHANDISE_REQUEST = 80001;
-	public final static int MERCHANDISE_RESPONSE = 80002;
-
-}
